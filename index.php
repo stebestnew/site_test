4bb747226e60ddfca8ed1f4050905128850e9363
stebestnew <ben_mouncef@hotmail.com>
2021-04-14 00:22:03 +0000

    add 2fichier

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="un">un</div>
+    
+    <div class="deux"> deux</div>
+    
+    <div class="trois">trois</div>
+    
+    <div class="quatre">quatre</div>
+
+    <p> je t'aime beuacoup </p>
+    
+</body>
+</html>
